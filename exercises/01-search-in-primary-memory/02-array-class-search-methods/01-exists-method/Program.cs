b3432306b2de693ba92afe66e56374e38236f893
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-09-08 13:52:52 -0300

    feat: adding search exercises using array class methods

---
+++ exercises/01-search-in-primary-memory/02-array-class-search-methods/01-exists-method/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExistsMethod
+{
+    class Student {
+        public string name;   
+        public int enrollmentCode;
+
+        public override String ToString(){
+            return name + " - " + enrollmentCode;
+        }
+    }
+
+    class Program
+    {
+        public static void Menu(){
+            Console.WriteLine("1 - Para cadastrar aluno");
+            Console.WriteLine("2 - Para pesquisar aluno");
+            Console.WriteLine("3 - Para mostrar todos alunos");
+            Console.WriteLine("4 - Para sair");
+        }
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            int option;
+            Menu();
+            option = Int32.Parse(Console.ReadLine());
+
+            while (option != 4) {
+                switch (option){
+                    case 1:
+                        Student student = new Student();
+
+                        Console.WriteLine("Informe o nome do aluno");
+                        student.name = Console.ReadLine();
+
+                        Console.WriteLine("Informe o número de matricula");
+                        student.enrollmentCode = Int32.Parse(Console.ReadLine());
+
+                        students.Add(student);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Informe o numero de matricula: ");
+
+                        int enrollmentCode = Int32.Parse(Console.ReadLine());
+
+                        if (students.Exists(item => item.enrollmentCode == enrollmentCode)) {
+                            Console.WriteLine("Aluno cadastrado!!!");
+                        } else {
+                            Console.WriteLine("Aluno não cadastrado!!!");
+                        }
+
+                        break;
+
+                    case 3:
+                        foreach (Student item in students) {
+                            Console.WriteLine(item.ToString());
+                        }
+
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+                Menu();
+                option = Int32.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ exercises/01-search-in-primary-memory/02-array-class-search-methods/02-find-method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] array = { "Rato", "Sapo", "Sapato", "Ratoeira", "Mato"};
+            String prefix;
+
+            Console.Write("Entre com o prefixo para pesquisa: ");
+            prefix = Console.ReadLine();
+
+            String[] response = Array.FindAll(array, item => item.StartsWith(prefix));
+
+            foreach(String word in response) {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
