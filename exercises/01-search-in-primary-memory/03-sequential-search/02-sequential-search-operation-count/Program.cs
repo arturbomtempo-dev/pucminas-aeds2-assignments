bc813e5093516f98948340a52da8416a3a7f5bc1
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-09-08 15:48:21 -0300

    feat: addition of sequential search exercises

---
+++ exercises/01-search-in-primary-memory/03-sequential-search/02-sequential-search-operation-count/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SequentialSearch
+{
+    class Program
+    {
+        static bool SequentialSearch(int[] array, int x)
+        {
+            bool found = false;
+            int operations = 0;
+
+            for (int i = 0; i < array.Length; i++) {
+                operations++;
+
+                if (array[i] == x) {
+                    found = true;
+                    i = array.Length;
+                }
+            }
+
+            Console.WriteLine($"Número de operações de verificação: {operations}");
+
+            return found;
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++) {
+                array[i] = random.Next(0, 101);
+            }
+
+            Console.WriteLine("Valores do array: ");
+            
+            foreach (int value in array) {
+                Console.Write(value + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número a ser pesquisado: ");
+            int number = int.Parse(Console.ReadLine());
+
+            bool result = SequentialSearch(array, number);
+
+            if (result) {
+                Console.WriteLine($"Número {number} encontrado no array.");
+            } else {
+                Console.WriteLine($"Número {number} não encontrado no array.");
+            }
+        }
+    }
+}
