717e145ade33a047989f0f1f79addced976b6d83
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-09-08 13:06:56 -0300

    feat: addition of exercises on arrays in C#

---
+++ exercises/01-search-in-primary-memory/01-arrays/02-string-array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringArray
+{
+    class Program
+    {
+        const int SIZE = 5;
+        
+        static void Main(string[] args)
+        {
+            string[] array = new string[SIZE];
+
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write("Entre com a " + (i + 1) + "a. palavra: ");
+                array[i] = Console.ReadLine();
+            }
+
+            Console.Write("Array com valores strings lidos: \n");
+            Console.Write("[");
+
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write(array[i] + ",");
+            }
+
+            Console.Write("]\n");
+        }
+    }
+}
---
+++ exercises/01-search-in-primary-memory/01-arrays/03-class-array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassArray
+{
+    class User {    
+        public string name;    
+        public int age;    
+        public string country;
+    }
+
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            User[] users = {
+                new User { name = "Homer Simpson", age = 42, country = "USA" },
+                new User { name = "Marge Simpson", age = 38, country = "USA"},
+                new User { name = "Bart Simpson", age = 19, country = "USA"},
+                new User { name = "Lisa Simpson", age = 17, country = "USA"}, 
+            };
+
+            foreach (User user in users) {
+                Console.WriteLine(user.name + " - " + user.age);
+            }
+        }
+    }
+}
