47c44e4b9646c45ecf0c4c270aadc32f9d8cd76a
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-09-18 18:46:37 -0300

    feat: addition of exercise 1120 from beecrowd

---
+++ beecrowd/1120/ContractReview.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ContractReview {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String D = sc.next();
+        String N = sc.next();
+
+        while (!(D.equals("0") && N.equals("0"))) {
+            char valueToRemove = D.charAt(0);
+            String newExpression = "";
+            boolean nonZeroFound = false;
+
+            for (int i = 0; i < N.length(); i++) {
+                char currentValue = N.charAt(i);
+
+                if (currentValue != valueToRemove) {
+                    if (currentValue != '0') {
+                        newExpression += currentValue;
+                        nonZeroFound = true;
+                    } else if (nonZeroFound) {
+                        newExpression += currentValue;
+                    }
+                }
+            }
+
+            if (newExpression.length() > 0) {
+                System.out.println(newExpression);
+            } else {
+                System.out.println("0");
+            }
+
+            D = sc.next();
+            N = sc.next();
+        }
+
+        sc.close();
+    }
+}
