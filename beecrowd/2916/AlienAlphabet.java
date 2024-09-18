c42c60c82b69352f955456a086981d6f68dad41b
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-09-18 16:19:28 -0300

    feat: addition of beecrowd exercise 2916

---
+++ beecrowd/2916/AlienAlphabet.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class AlienAlphabet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int K, N, countCharsDiff = 0;
+
+        K = sc.nextInt();
+        N = sc.nextInt();
+
+        char[] alienAlphabet = new char[K];
+        char[] expression = new char[N];
+
+        String alienChars = sc.next();
+
+        for (int i = 0; i < K; i++) {
+            alienAlphabet[i] = alienChars.charAt(i);
+        }
+
+        String expr = sc.next();
+
+        for (int j = 0; j < N; j++) {
+            expression[j] = expr.charAt(j);
+        }
+
+        for (int i = 0; i < N; i++) {
+            char currentChar = expression[i];
+            boolean foundInAlphabet = false;
+
+            for (int j = 0; j < K; j++) {
+                if (currentChar == alienAlphabet[j]) {
+                    foundInAlphabet = true;
+                }
+            }
+
+            if (!foundInAlphabet) {
+                countCharsDiff++;
+            }
+        }
+
+        if (countCharsDiff == 0) {
+            System.out.println("S");
+        } else {
+            System.out.println("N");
+        }
+
+        sc.close();
+    }
+}
