4a05b53f274b43ec385be329b8dbf9dfb60fb33c
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-10-10 00:01:18 -0300

    chore: adjustments to the C struct code of the second practical work in order to make it easier to read and operate

--- practical-assignments/02-practical-assignment/02-pokemon-struct/Main.c
+++ practical-assignments/02-practical-assignment/02-pokemon-struct/Main.c
@@ -1,253 +1,288 @@
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * TP02Q02 - Registro em C
  * 
  * @author Artur Bomtempo Colen
- * @version 1.0, 29/09/2024
+ * @version 2.0, 10/10/2024
  */
 
 /**
+ * @struct Date
+ * @brief Estrutura usada para representar a data de captura de um Pokémon.
+ * 
+ * A estrutura Date armazena informações de uma data, composta por dia, mês e ano,
+ * facilitando a formatação e manipulação de datas relacionadas à captura de Pokémon.
+ */
+struct Date {
+    int day;
+    int month;
+    int year;
+} typedef Date;
+
+/**
  * @struct Pokemon
- * @brief Estrutura que representa um Pokémon.
- *
- * Esta estrutura contém informações detalhadas sobre um Pokémon, incluindo 
- * seus atributos básicos.
+ * @brief Estrutura usada para representar um Pokémon e suas principais características.
+ * 
+ * A estrutura Pokemon contém informações detalhadas sobre um Pokémon, incluindo seu 
+ * identificador único, atributos físicos, habilidades, taxa de captura, geração e a data de captura.
  */
 struct Pokemon {
     int id;
     int generation;
     char name[100];
     char description[100];
-    char types[2][100];
-    char abilities[10][100];
-    double weightKg;
-    double heightM;
+    char type[2][100];
+    char abilities[20][100];
+    double weight;
+    double height;
     int captureRate;
-    bool isLegendary;
-    struct tm captureDate;
+    bool isLegendary;  
+    Date captureDate;
 } typedef Pokemon;
 
 /**
- * Exibe as informações detalhadas de um Pokémon.
+ * Esta função percorre uma lista de Pokémons e busca aquele que possui o ID fornecido. 
+ * Retorna um ponteiro para o Pokémon encontrado.
+ * 
+ * @param pokemons Um ponteiro para a lista de Pokémons.
+ * @param id O ID do Pokémon a ser buscado.
+ * @return Um ponteiro para o Pokémon encontrado. Se o ID não for encontrado, retorna NULL.
+ */
+Pokemon *searchPokemon(Pokemon *pokemons, int id) {
+    Pokemon *pokemon;
+    
+    for (int i = 0; i < 801; i++) {
+        if (pokemons[i].id == id) {
+            pokemon = &pokemons[i];
+        }
+    }
+    
+    return pokemon; 
+}
+
+/**
+ * Esta função percorre a string original e remove todas as ocorrências do caractere
+ * fornecido, deslocando os demais caracteres para preencher os espaços removidos.
+ * 
+ * @param str A string original a ser modificada.
+ * @param value O caractere a ser removido da string.
+ */
+void removeOccurrences(char *str, char value) {
+    int i, j = 0;
+    size_t length = strlen(str);
+    
+    for (i = 0; i < length; i++) {
+        
+        if (str[i] != value) {
+            str[j++] = str[i];
+        }
+    }
+
+    str[j] = '\0';
+}
+
+/**
+ * A função busca pelo próximo delimitador (ou aspas, se for o caso) na string fornecida.
+ * O trecho encontrado é removido da string original e retornado. A string original
+ * é atualizada para continuar após o delimitador encontrado.
+ * 
+ * @param str Um ponteiro para a string original que será modificada. Após a execução, o ponteiro será atualizado para apontar para o próximo segmento da string.
+ * @param delimiter O caractere delimitador que indica o fim do atributo a ser removido.
+ * @return O atributo removido da string original. 
+ */
+char *removeAttribute(char **str, char delimiter) {
+    char *start = *str;
+    char *end = *str;
+
+    if (*start == '"') {
+        start++;
+        end = strchr(start, '"');
+    } else {
+        end = strchr(start, delimiter);
+    }
+
+    if (end != NULL) {
+        *end = '\0';
+        *str = end + 1;
+    } else {
+        *str = start + strlen(start);
+    }
+
+    return start;
+}
+
+/**
+ * A função aloca dinamicamente um array de estruturas `Pokemon` para armazenar até 801 Pokémons.
+ * Em seguida, ela lê as linhas do arquivo CSV, extrai os dados de cada Pokémon e preenche
+ * o array com essas informações. Se houver erro de alocação de memória ou na abertura do
+ * arquivo, mensagens de erro serão exibidas.
  *
- * @param pokemon Estrutura do Pokémon a ser exibida.
+ * @param fileName O nome do arquivo CSV a ser lido.
+ * @return Um ponteiro para o array de Pokémons preenchido com os dados do arquivo. 
+ *         Se ocorrer erro, o retorno será `NULL`.
+ */
+Pokemon *readCsv(char fileName[]) {
+    Pokemon *pokemons = (Pokemon*) malloc(801 * sizeof(Pokemon));
+
+    if (!pokemons) {
+        printf("Erro de alocação de memória.\n");
+    }
+
+    FILE *file = fopen(fileName, "r");
+
+    if (!file) {
+        printf("Erro ao abrir o arquivo.\n");
+        free(pokemons);
+    }
+
+    char line[500];
+
+    fgets(line, sizeof(line), file);
+
+    int index = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL && index < 801) {
+        Pokemon *temp = &pokemons[index];
+        char *token = line;
+
+        temp->id = atoi(removeAttribute(&token, ','));
+        temp->generation = atoi(removeAttribute(&token, ','));
+        strcpy(temp->name, removeAttribute(&token, ','));
+        strcpy(temp->description, removeAttribute(&token, ','));
+        strcpy(temp->type[0], removeAttribute(&token, ','));
+        strcpy(temp->type[1], removeAttribute(&token, ','));
+
+        char *abilities = removeAttribute(&token, ','); 
+
+        removeOccurrences(abilities, '[');
+        removeOccurrences(abilities, ']');
+        removeOccurrences(abilities, '\'');
+
+        char *tokenAbilities = abilities;
+        int abilityIndex = 0;
+
+        while (*tokenAbilities != '\0' && abilityIndex < 10) {
+            while (isspace(*tokenAbilities)) {
+                tokenAbilities++;
+            }
+
+            char *startAbility = tokenAbilities;
+            char *endAbility;
+
+            if (*startAbility == '\'') {
+                startAbility++;
+                endAbility = strchr(startAbility, '\'');
+            } else {
+                endAbility = strchr(startAbility, ','); 
+            }
+
+            if (endAbility != NULL) {
+                *endAbility = '\0'; 
+                strcpy(temp->abilities[abilityIndex], startAbility);
+                abilityIndex++;
+                tokenAbilities = endAbility + 1;
+            } else {
+                strcpy(temp->abilities[abilityIndex], startAbility);
+                break;
+            }
+        }
+
+        char *trash = removeAttribute(&token, ',');
+
+        temp->weight = atof(removeAttribute(&token, ','));
+        temp->height = atof(removeAttribute(&token, ','));
+        temp->captureRate = atoi(removeAttribute(&token, ','));
+
+        int isLegendary = atoi(removeAttribute(&token, ','));
+
+        if (isLegendary == 0) {
+            temp->isLegendary = false;
+        } else {
+            temp->isLegendary = true;
+        }
+
+        sscanf(removeAttribute(&token, '/'), "%d", &temp->captureDate.day);
+        sscanf(removeAttribute(&token, '/'), "%d", &temp->captureDate.month);
+        sscanf(removeAttribute(&token, '/'), "%d", &temp->captureDate.year);
+
+        index++;
+    }
+
+    fclose(file);
+
+    return pokemons;
+}
+
+/**
+ * A função imprime os dados do Pokémon, como ID, nome, descrição, tipos,
+ * habilidades, peso, altura, taxa de captura, se é lendário ou não, geração,
+ * e data de captura em um formato padronizado.
+ *
+ * @param pokemon A estrutura Pokemon cujas informações serão exibidas.
  */
 void displayInformation(Pokemon pokemon) {
-    char dateBuffer[11];
-    
-    dateFormat(&pokemon.captureDate, dateBuffer);
+    printf("[#%d -> %s: %s - ", pokemon.id, pokemon.name, pokemon.description);
 
-    printf("[#%d -> %s: %s - [", pokemon.id, pokemon.name, pokemon.description);
+    printf("['%s'", pokemon.type[0]);
 
-    for (int i = 0; i < 2; i++) {
-        if (strlen(pokemon.types[i]) > 0) {
-            printf("'%s'%s", pokemon.types[i], (i < 2 - 1 && strlen(pokemon.types[i + 1]) > 0) ? ", " : "");
-        }
+    if (strlen(pokemon.type[1]) > 0) {
+        printf(", '%s'", pokemon.type[1]);
     }
 
     printf("] - [");
 
-    for (int i = 0; i < 10; i++) {
-        if (strlen(pokemon.abilities[i]) > 0) {
-            printf("'%s'%s", pokemon.abilities[i], (i < 10 - 1 && strlen(pokemon.abilities[i + 1]) > 0) ? ", " : "");
+    for (int i = 0; strlen(pokemon.abilities[i]) > 0; i++) {
+        printf("'%s'", pokemon.abilities[i]);
+
+        if (strlen(pokemon.abilities[i + 1]) > 0) {
+            printf(", ");
         }
     }
 
-    printf("] - %.1fkg - %.1fm - %d%% - %s - %d gen] - %s\n", pokemon.weightKg, pokemon.heightM, pokemon.captureRate, pokemon.isLegendary ? "true" : "false", pokemon.generation, dateBuffer);
+    printf("] - ");
+
+    printf("%.1lfkg - %.1lfm - %d%% - %s - %d gen] - %02d/%02d/%d\n", 
+           pokemon.weight, 
+           pokemon.height, 
+           pokemon.captureRate, 
+           pokemon.isLegendary ? "true" : "false", 
+           pokemon.generation,
+           pokemon.captureDate.day, 
+           pokemon.captureDate.month, 
+           pokemon.captureDate.year);
 }
 
 /**
- * A função abre o arquivo especificado, ignora a primeira linha (cabeçalho) e lê cada linha
- * subsequente para preencher a estrutura `Pokemon`. Ela verifica se o ID do Pokémon está na
- * lista de IDs buscados e, em caso afirmativo, armazena os dados do Pokémon na lista de Pokémon.
- * Se ocorrer algum erro na leitura de uma linha, uma mensagem de erro é exibida.
- *
- * @param filename Nome do arquivo CSV a ser lido.
- * @param searchedIDS Array contendo os IDs de Pokémon que devem ser buscados no arquivo.
- * @param count Número total de IDs a serem buscados.
- */
-void readCSV(const char *filename, int searchedIDS[], int count) {
-    FILE *file = fopen(filename, "r");
-    
-    if (file) {
-        char line[100 * 20];
-
-        fgets(line, sizeof(line), file);
-
-        Pokemon pokemons[150] = {0};
-        bool foundIDS[150] = {false};
-        int foundPokemons = 0;
-
-        while (fgets(line, sizeof(line), file) && foundPokemons < count) {
-            Pokemon pokemon = { 0 };
-
-            char abilities[100] = { 0 };
-            char type1[100] = { 0 };
-            char type2[100] = { 0 };
-            char captureDate[100] = { 0 };
-            int isLegendaryTemp = { 0 };
-
-            if (sscanf(line, "%d,%d,%99[^,],%99[^,],%99[^,],%99[^,],\"%199[^\"]\",%lf,%lf,%d,%d,%99[^\n]", &pokemon.id, &pokemon.generation, pokemon.name, pokemon.description, type1, type2, abilities, &pokemon.weightKg, &pokemon.heightM, &pokemon.captureRate, &isLegendaryTemp, captureDate) != 12) {
-                if (sscanf(line, "%d,%d,%99[^,],%99[^,],%99[^,],,\"%199[^\"]\",%lf,%lf,%d,%d,%99[^\n]", &pokemon.id, &pokemon.generation, pokemon.name, pokemon.description, type1, abilities, &pokemon.weightKg, &pokemon.heightM, &pokemon.captureRate, &isLegendaryTemp, captureDate) != 11) {
-                    if (sscanf(line, "%d,%d,%99[^,],%99[^,],%99[^,],%99[^,],\"%199[^\"]\",,,%d,%d,%99[^\n]", &pokemon.id, &pokemon.generation, pokemon.name, pokemon.description, type1, type2, abilities, &pokemon.captureRate, &isLegendaryTemp, captureDate) != 10) {
-                        if (sscanf(line, "%d,%d,%99[^,],%99[^,],%99[^,],,\"%199[^\"]\",,,%d,%d,%99[^\n]", &pokemon.id, &pokemon.generation, pokemon.name, pokemon.description, type1, abilities, &pokemon.captureRate, &isLegendaryTemp, captureDate) != 9) {
-                            printf("Erro ao ler a line: %s\n", line);
-                        }
-                    }
-                }
-            }
-
-            int passPokemon = 1;
-
-            for (int i = 0; i < count; i++) {
-                if (pokemon.id == searchedIDS[i]) {
-                    passPokemon = 0;
-                    break;
-                }
-            }
-
-            if (passPokemon) {
-                continue;
-            }
-
-            pokemon.isLegendary = isLegendaryTemp ? true : false;
-            strncpy(pokemon.types[0], type1, 100);
-
-            if (strlen(type2) > 0) {
-                strncpy(pokemon.types[1], type2, 100);
-            } else {
-                pokemon.types[1][0] = '\0';
-            }
-
-            abilities[strcspn(abilities, "\n")] = '\0';
-
-            char *start = strchr(abilities, '[');
-            char *end = strchr(abilities, ']');
-
-            if (start) {
-                start++;
-            }
-
-            if (end) {
-                *end = '\0';
-            }
-
-            char *token = strtok(start, ",");
-            int abilityIndex = 0;
-
-            while (token != NULL && abilityIndex < 10) {
-                char cleanAbility[100];
-
-                sscanf(token, " '%[^\']'", cleanAbility);
-                strncpy(pokemon.abilities[abilityIndex++], cleanAbility, 100);
-                token = strtok(NULL, ",");
-            }
-
-            int day, month, year;
-
-            sscanf(captureDate, "%d/%d/%d", &day, &month, &year);
-
-            pokemon.captureDate.tm_year = year - 1900;
-            pokemon.captureDate.tm_mon = month - 1;
-            pokemon.captureDate.tm_mday = day;
-
-            for (int i = 0; i < count; i++) {
-                if (searchedIDS[i] == pokemon.id && !foundIDS[i]) {
-                    pokemons[i] = pokemon;
-                    foundIDS[i] = true;
-                    foundPokemons++;
-
-                    break;
-                }
-            }
-        }
-
-        fclose(file);
-
-        for (int i = 0; i < count; i++) {
-            if (foundIDS[i]) {
-                displayInformation(pokemons[i]);
-            }
-        }
-    }
-}
-
-/**
- * Esta função recebe um Pokémon original e cria uma nova instância
- * do Pokémon com os mesmos atributos. Todos os campos do Pokémon original,
- * incluindo ID, geração, nome, descrição, tipos, habilidades, peso,
- * altura, taxa de captura, status de lendário e data de captura, são
- * copiados para o Pokémon clonado. 
- *
- * @param original O Pokémon a ser clonado.
- * @return Um novo objeto `Pokemon` que é uma cópia do Pokémon original.
- */
-Pokemon clonePokemon(const Pokemon original) {
-    Pokemon clone;
-
-    clone.id = original.id;
-    clone.generation = original.generation;
-
-    strncpy(clone.name, original.name, 100);
-    strncpy(clone.description, original.description, 100);
-
-    for (int i = 0; i < 2; i++) {
-        strncpy(clone.types[i], original.types[i], 100);
-    }
-
-    for (int i = 0; i < 10; i++) {
-        strncpy(clone.abilities[i], original.abilities[i], 100);
-    }
-
-    clone.weightKg = original.weightKg;
-    clone.heightM = original.heightM;
-    clone.captureRate = original.captureRate;
-    clone.isLegendary = original.isLegendary;
-    clone.captureDate = original.captureDate;
-
-    return clone;
-}
-
-/**
- * Esta função recebe uma estrutura `tm` que contém uma data e formata
- * essa data em uma string no formato "dd/mm/aaaa". A string formatada
- * é armazenada no buffer fornecido.
- *
- * @param date   Um ponteiro para a estrutura `tm` que contém a data a ser formatada.
- * @param buffer Um ponteiro para a string onde a data formatada será armazenada.
- */
-void dateFormat(struct tm *date, char *buffer) {
-    strftime(buffer, 11, "%d/%m/%Y", date);
-}
-
-/**
- * Esta função inicializa a execução do programa, permitindo que o usuário insira
- * IDs de Pokémon que deseja pesquisar. Os IDs são armazenados em um vetor e, 
- * quando o usuário digita "FIM", a função `readCSV` é chamada para ler os dados
- * dos Pokémon a partir de um arquivo CSV e exibir as informações dos Pokémon
- * correspondentes aos IDs fornecidos.
+ * Função principal do programa, que gerencia a leitura de dados de Pokémon
+ * e permite a busca e exibição de informações sobre os Pokémon com base
+ * em IDs fornecidos pelo usuário.
  */
 int main() {
-    int searchedIDS[150];
-    int count = 0;
+    int id;
+    char input[20];
+    
+    Pokemon *pokemons = readCsv("tmp/pokemon.csv");
 
-    char input[100] = { 0 };
+    if (!pokemons) {
+        printf("Pokemons não inicializados.\n");
+    }
+    
+    while (scanf("%s", input) && strcmp(input, "FIM")) {
+        sscanf(input, "%d", &id);
 
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0';
-
-    while (strcmp(input, "FIM") != 0) {
-        searchedIDS[count++] = atoi(input);
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = '\0';
+        Pokemon *foundPokemon = searchPokemon(pokemons, id);
+        
+        if (id <= 801) {
+            displayInformation(*foundPokemon);
+        }
     }
 
-    readCSV("/tmp/pokemon.csv", searchedIDS, count);
+    free(pokemons);
 
     return 0;
 }
